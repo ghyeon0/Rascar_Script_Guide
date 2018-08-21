8d4483a6ade70a71a3f3c10308eae379e3e26c57
ghyeon0 <xcm1321@gmail.com>
2018-08-21 15:56:35 +0900

    shell 스크립트 제거

--- display_ssh_set.sh
+++
@@ -1,9 +0,0 @@
-cd /media/$USER/boot
-touch ssh
-echo "" >> config.txt
-echo "max_usb_current=1" >> config.txt
-echo "hdmi_group=2" >> config.txt
-echo "hdmi_mode=1" >> config.txt
-echo "hdmi_mode=87" >> config.txt
-echo "hdmi_cvt 800 480 60 6 0 0 0" >> config.txt
-echo "hdmi_drive=1" >> config.txt
